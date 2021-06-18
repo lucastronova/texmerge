9d7747f810255833cb6f643bb579a3b71acab688
teddygonyea <62407086+teddygonyea@users.noreply.github.com>
2021-06-17 23:16:42 -0400

    checked for outfile

--- func.hpp
+++ func.hpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sys/stat.h>
+
 
 typedef unsigned long long	u128;
 typedef unsigned long		u64;
@@ -11,16 +13,31 @@ typedef unsigned char		u8;
 
 int extract( u128* masks, u8 amount, std::string infile, std::string outfile )
 {
-	/*
 	
-	1. make new folder with name "outfile"
-	2. make subfolders with proper names
-	3. for each subfolder:
-		a. check the bitmask:
-			i. if 0: create appropriate blank file
-			ii. if 1: copy from source
 	
-	*/
+	//1. make new folder with name "outfile
+    
+    if (!mkdir(outfile, 0777))
+    {
+        return -1;
+    }
+    
+    //newdir = outfile + /newdir
+    
+    
+    
+	//2. make subfolders with proper names
+    
+    
+    
+	//3. for each subfolder:
+		//a. check the bitmask:
+			//i. if 0: create appropriate blank file
+			//ii. if 1: copy from source
+	
+
 	
 	
 }
+
+
